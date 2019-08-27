9c4c48c48c7342540b6f6d365e9a453cfc9a32b8
Julia Hellen Ferreira <38631847+JuliaHellenFerreira@users.noreply.github.com>
2019-08-27 13:04:32 -0300

    Add files via upload

---
+++ Analisesimples.R
@@ -0,0 +1,50 @@
+# Analisando
+
+BD_bilheteria2
+
+## Filtrar os meses
+
+MesJan <- BD_bilheteria2[BD_bilheteria2$..1 == "January",]
+MesJan
+MesFeb <- BD_bilheteria2[BD_bilheteria2$..1 == "February",]
+MesFeb
+MesMar <- BD_bilheteria2[BD_bilheteria2$..1 == "March",]
+MesMar
+MesApr <- BD_bilheteria2[BD_bilheteria2$..1 == "April",]
+MesApr
+MesMay <- BD_bilheteria2[BD_bilheteria2$..1 == "May",]
+MesMay
+MesJun <- BD_bilheteria2[BD_bilheteria2$..1 == "Jun",]
+MesJun
+MesJul <- BD_bilheteria2[BD_bilheteria2$..1 == "July",]
+MesJul
+MesAug <- BD_bilheteria2[BD_bilheteria2$..1 == "August",]
+MesAug
+
+# Soma do lucro total de cada mês
+
+sum(MesJan$`Total Gross`)
+sum(MesFeb$`Total Gross`)
+sum(MesMar$`Total Gross`)
+sum(MesApr$`Total Gross`)
+sum(MesMay$`Total Gross`)
+sum(MesJun$`Total Gross`)
+sum(MesJul$`Total Gross`)
+sum(MesAug$`Total Gross`)
+
+# Resumo dos meses
+
+summary(MesJan$`Total Gross`)
+summary(MesFeb$`Total Gross`)
+summary(MesMar$`Total Gross`)
+summary(MesMay$`Total Gross`)
+summary(MesJun$`Total Gross`)
+summary(MesJul$`Total Gross`)
+summary(MesAug$`Total Gross`)
+
+# Países e seus lucros mensais ### Teste ###
+
+x <- BD_bilheteria2[BD_bilheteria2$..1 == "January" && BD_bilheteria2$Country == "Brazil",]
+x
+
+
