eb1e180beefa3a764323f8ef0e3261d0946c60d3
Julia Hellen Ferreira <38631847+JuliaHellenFerreira@users.noreply.github.com>
2019-09-03 11:46:21 -0300

    Add files via upload

--- AnaliseTidyverse.R
+++ AnaliseTidyverse.R
@@ -4,31 +4,34 @@ library(tidyverse)
 
 # Filtrar os países e os meses
 
-base1 = BD_Bilheteria2_20_202018 %>% 
+base = BD_bilheteria2 %>% 
   group_by(Country,Weekly) %>% 
   
 # Simplificar o eixo Y
-
+  
 summarise(TotalGross = sum(`Total Gross`/1000000))
 
-BaseBrazil = base1 %>% 
-  filter(Country == "USA")
+############################### Bilheterias Mensais por Países#######################################
 
-BaseBrazil
+pais <- function(data,p = "Brazil"){
+Base1 = base %>% 
+filter(Country == p)
+  
+# Ordenar os meses
 
-# Ordenar os meses 
-
-BaseBrazil$Weekly = ordered(BaseBrazil$Weekly, levels = c("January","February","March","April","May",
-                                                          "June","July",
-                                                          "August","September",
-                                                          "October","November",
-                                                          "December"))
-# Fazer gráficos 
-
-## Gráfico de Barras
-
-ggplot(BaseBrazil,aes(x = Weekly, y = TotalGross))+
+Base1$Weekly = ordered(Base1$Weekly, levels = c("January","February","March","April","May",
+                                                            "June","July",
+                                                            "August","September",
+                                                            "October","November",
+                                                            "December"))
+# Fazer gráficos - Brazil
+  
+## Gráfico de Barras - Brazil
+  
+barras <- ggplot(Base1,aes(x = Weekly, y = TotalGross))+
   geom_bar(stat = "identity", col = "black", fill = "steelblue")+
-  labs(x = "Meses", y = "Bilheteria", title = "Bilheteria Mensal dos filmes nos USA em 2018 em Milhões de Dólares")
-
-
+  labs(x = "Meses", y = "Bilheteria", title = paste0("Bilheteria Mensal dos filmes nos ", p, " em 2019 em Milhões de Dólares"))
+  listadegraficos <- list(barras)
+  return(listadegraficos)
+}
+pais(BD_bilheteria2, p = "Australia")
